47144cf8e999bb3878ee5a2a724d4e3d730f2945
BigBigBai <baiyingjian@outlook.com>
2025-10-28 07:52:45 -0400

    add LocalStorage feature

--- 04_mini_projects/notes-app/src/App.jsx
+++ 04_mini_projects/notes-app/src/App.jsx
@@ -1,14 +1,22 @@
 import NoteForm from './components/NoteForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NoteList from './components/NoteList';
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const savedNotes = localStorage.getItem('notes');
+    return savedNotes ? JSON.parse(savedNotes) : [];
+  });
 
   //Handle deleting a note
   const deleteNote = (id) => {
     setNotes(notes.filter((note) => note.id !== id));
   };
+
+  // Save notes to localStorage whenever notes state changes
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg'>
