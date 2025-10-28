d43eebde6178decd86a0bcf0a43caebeee155827
BigBigBai <baiyingjian@outlook.com>
2025-10-27 21:43:01 -0400

    create Button.jsx

---
+++ 03_mini_projects/rating-ui/src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ children, onClick, className, disabled = false }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
