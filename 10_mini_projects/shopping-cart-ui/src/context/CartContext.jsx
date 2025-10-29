67c6c8125d2c4688fc3e9078193aa88896c0ffa5
BigBigBai <baiyingjian@outlook.com>
2025-10-29 18:16:10 -0400

    add Cart Context

---
+++ 10_mini_projects/shopping-cart-ui/src/context/CartContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return <CartContext.Provider value={{}}>{children}</CartContext.Provider>;
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
