77315f8edfca4b980a4680b9499ecef0793e7392
BigBigBai <baiyingjian@outlook.com>
2025-10-29 18:09:34 -0400

    add Products Context

--- 10_mini_projects/shopping-cart-ui/src/components/ProductList.jsx
+++ 10_mini_projects/shopping-cart-ui/src/components/ProductList.jsx
@@ -1,6 +1,17 @@
 import ProductCard from './ProductCard';
+import { useProducts } from '../context/ProductContext';
 
-const ProductList = ({ products }) => {
+const ProductList = () => {
+  const { products, loading, error } = useProducts();
+
+  {
+    loading && <p>Loading...</p>;
+  }
+
+  {
+    error && <p className='text-red-500'>❌ {error}</p>;
+  }
+
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
       {products.map((product) => (
