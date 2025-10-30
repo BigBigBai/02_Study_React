6c725c8c88357f70d26775c36c1e08b43d4739c8
BigBigBai <baiyingjian@outlook.com>
2025-10-29 23:09:28 -0400

    create Routes

---
+++ 11_projects/friendly-dev/app/routes/home/index.tsx
@@ -0,0 +1,22 @@
+// import type { Route } from '../+types/home';
+
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: 'The Friendly Dev Portfolio' },
+//     { name: 'description', content: 'Web design and development projects' },
+//   ];
+// }
+
+// export default function Home() {
+//   return <div>My App</div>;
+// }
+
+const HomePage = () => {
+  return (
+    <section>
+      <h1>Welcome</h1>
+    </section>
+  );
+};
+
+export default HomePage;
