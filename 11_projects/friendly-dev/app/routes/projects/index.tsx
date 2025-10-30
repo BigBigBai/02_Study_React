5f90a2ab8a4df281a813a322d4c69149eb02e9ac
BigBigBai <baiyingjian@outlook.com>
2025-10-30 10:23:48 -0400

    use loader data

--- 11_projects/friendly-dev/app/routes/projects/index.tsx
+++ 11_projects/friendly-dev/app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import type { Route } from './+types/index';
 import type { Project } from '~/types';
+import ProjectCard from '~/components/ProjectCard';
 
 export async function loader({
   request,
@@ -12,12 +13,18 @@ export async function loader({
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
-  console.log(projects);
+  //   console.log(projects);
 
   return (
-    <section>
-      <h2 className='text-3xl font-bold mb-8 text-white'>Projects</h2>
-    </section>
+    <>
+      <h2 className='text-3xl font-bold mb-8 text-white'>🚀 Projects</h2>
+
+      <div className='grid gap-6 sm:grid-cols-2'>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </>
   );
 };
 
